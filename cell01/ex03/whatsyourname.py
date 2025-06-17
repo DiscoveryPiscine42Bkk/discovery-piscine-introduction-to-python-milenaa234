cd97b4f1f77fde202739d70ecc0f102355d487a5
milenaa234 <charukansombut@gmail.com>
2025-06-17 12:02:34 +0000

    add number zero

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,14 @@
+name = "fang"
+last_name = "charukan"
+
+s = f"Hey, what's your first name? : {name}"
+
+print(s)
+
+o = f"And your last name? : {last_name}"
+
+print(o)
+
+y = f"Well, pleasd to meet you, {name} {last_name}"
+
+print(y)
---
+++ cell02/ex02/password.py
@@ -0,0 +1,6 @@
+prampt = input()22
+passeord = input("Python is awesome:")
+if prampt == password
+    print("Access granted")
+else:
+    print("Access granted")
